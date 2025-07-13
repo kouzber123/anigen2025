432c9ff4c01b0d9db2e7f63f0c04f4d11183d905
kouzber123 <tom.nguyen@windowslive.com>
2025-07-13 20:36:12 +0300

    Feat: initial commit + setup

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
